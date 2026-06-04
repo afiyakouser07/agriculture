19fb66dac5698037ce91fe9d99f792f97043a935
afiyakouser07 <afiyakouser07@gmail.com>
2026-06-04 13:17:25 +0530

    Update 1_Data_Overview.py

--- pages/1_Data_Overview.py
+++ pages/1_Data_Overview.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Load Dataset
-df = pd.read_csv("data/agriculture_data.csv")
+df = pd.read_csv("data/data_season.csv")
 
 st.set_page_config(
     page_title="Data Overview",
