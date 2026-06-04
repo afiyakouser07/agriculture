89048c311c8f56c8b846ead24fb6509158624189
afiyakouser07 <afiyakouser07@gmail.com>
2026-06-04 13:11:17 +0530

    Update 2_Crop_Analytics.py

--- pages/2_Crop_Analytics.py
+++ pages/2_Crop_Analytics.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 # Load Dataset
-df = pd.read_csv("data/agriculture_data.csv")
+df = pd.read_csv("data/data_season.csv")
 
 st.set_page_config(
     page_title="Crop Analytics",
